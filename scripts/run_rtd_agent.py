c8138242ea61e363a47b31593f641f820bc2210a
Richard <richard@rghsoftware.co.uk>
2025-01-08 16:17:08 +0000

    flexible script

--- scripts/run_rtd_agent.py
+++ scripts/run_rtd_agent.py
@@ -1,8 +1,27 @@
+#!/usr/bin/env python
+
 from agentfit.rtd_agent import RTDAgent
 
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument(
+    "task",
+    type=str,
+)
+
+parser.add_argument(
+    "--rtd-url",
+    type=str,
+    default="https://pyautofit.readthedocs.io/en/latest/",
+)
+
+args = parser.parse_args()
+
 rtd_agent = RTDAgent(
-    "https://pyautofit.readthedocs.io/en/latest/",
-    "Write a simple script for fitting a 1D Gaussian",
+    args.rtd_url,
+    args.task,
 )
 
 print(rtd_agent.run())
